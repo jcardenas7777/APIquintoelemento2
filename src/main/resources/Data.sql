1c1c002399c7bd4bdfe3d9b64e0cff976013c691
StivenP31 <110734160+StivenP31@users.noreply.github.com>
2022-10-25 11:32:09 -0500

    si

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,3 +1,6 @@
 INSERT IGNORE INTO rol(id, nombre_rol) VALUES(1, "Administrador"),
                                             (2, "Doctor"),
                                             (3, "Recepcion");
+
+
+INSERT IGNORE
